9512da6c9f2785f0d70215015b9f8c98f60a7b93
murrayb2016 <murrayb2016@yahoo.com>
2016-08-05 11:00:55 -0500

    angularjs youtube

--- src/EnTag/wwwroot/ngApp/app.ts
+++ src/EnTag/wwwroot/ngApp/app.ts
@@ -1,6 +1,6 @@
 namespace EnTag {
 
-    angular.module('EnTag', ['ui.router', 'ngResource', 'ui.bootstrap']).config((
+    angular.module('EnTag', ['ui.router', 'ngResource', 'ui.bootstrap', 'youtube-embed']).config((
         $stateProvider: ng.ui.IStateProvider,
         $urlRouterProvider: ng.ui.IUrlRouterProvider,
         $locationProvider: ng.ILocationProvider
