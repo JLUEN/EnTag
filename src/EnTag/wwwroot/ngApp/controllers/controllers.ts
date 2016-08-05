ba19f9a496acafc885cea0c5b0b7d1ec76600f6d
murrayb2016 <murrayb2016@yahoo.com>
2016-08-05 15:02:57 -0500

    testing search youtube

--- src/EnTag/wwwroot/ngApp/controllers/controllers.ts
+++ src/EnTag/wwwroot/ngApp/controllers/controllers.ts
@@ -31,7 +31,7 @@ namespace EnTag.Controllers {
     export class AboutController {
         public message = 'Hello from the about page!';
 
-        public theBestVideo = '4aK1GK1KBGc';
+        public theBestVideo = "wtfHs-LREj0"
     }
 
 }
