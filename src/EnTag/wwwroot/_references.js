cb49f8388da84ef39d0196937472dfbe1f17dd95
Paul Cornwell <wolfking19@aol.com>
2016-08-09 09:40:31 -0500

    Added altControllers so people can work on controllers at same time

--- src/EnTag/wwwroot/_references.js
+++ src/EnTag/wwwroot/_references.js
@@ -17,5 +17,6 @@
 /// <reference path="ngapp/app.js" />
 /// <reference path="ngapp/controllers/accountcontroller.js" />
 /// <reference path="ngapp/controllers/controllers.js" />
+/// <reference path="ngapp/controllers/homecontroller.js" />
 /// <reference path="ngapp/services/accountservice.js" />
 /// <reference path="ngapp/services/services.js" />
---
+++ src/EnTag/wwwroot/ngApp/controllers/altControllers.ts
@@ -0,0 +1,16 @@
+﻿namespace EnTag.Controllers {
+    export class HomeController {
+        public message = 'Hello from the home page!';
+
+        public tweets;
+
+        //constructor(private $http: ng.IHttpService) {
+        //    $http.get('/api/test')
+        //        .then((response) => {
+        //            this.tweets = response.data;
+        //        });
+        //}
+
+
+    }
+}
